7674ff8eae032a4e8ef18b3b8984899b45b717d1
Alfio Emanuele Fresta <alfio.emanuele.f@gmail.com>
2016-08-09 11:53:59 +0100

    Added list_of_words function (no dictionary yet)

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from distutils.core import setup, Extension
 
-cp_otp_mod = Extension('cp_otp', sources = ['cp_otp.c'])
 
-setup(name = "cp_otp",
-    version = "0.1",
-    description = "Cyber Practicals One-Time Pad Extension Library",
-    ext_modules = [cp_otp_mod],
-)
+cp_otp_mod = Extension('cp_otp', sources=['cp_otp.c'])
+
+setup(name="cp_otp",
+      version="0.1",
+      description="Cyber Practicals One-Time Pad Extension Library",
+      ext_modules=[cp_otp_mod])
